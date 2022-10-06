bdbf0a4550d74e47b07ea1b0be021b4db90686fc
RosenMitrov <rdatabase.m@gmail.com>
2022-10-06 10:31:36 +0300

    Added Google Car

---
+++ ad06DefiningClasses/exercises/P07Google/Car.java
@@ -0,0 +1,16 @@
+package JavaAdvanced.ad06DefiningClasses.exercises.P07Google;
+
+public class Car {
+    private String carModel;
+    private int carSpeed;
+
+    public Car(String carModel, int carSpeed) {
+        this.carModel = carModel;
+        this.carSpeed = carSpeed;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %d", this.carModel, this.carSpeed);
+    }
+}
