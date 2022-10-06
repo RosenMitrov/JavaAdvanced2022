81bf1067f56a8f789304b4948511854246a514a5
RosenMitrov <rdatabase.m@gmail.com>
2022-10-06 10:32:55 +0300

    Added Google Pokemon

---
+++ ad06DefiningClasses/exercises/P07Google/Pokemon.java
@@ -0,0 +1,16 @@
+package JavaAdvanced.ad06DefiningClasses.exercises.P07Google;
+
+public class Pokemon {
+    private String pokemonName;
+    private String pokemonType;
+
+    public Pokemon(String pokemonName, String pokemonType) {
+        this.pokemonName = pokemonName;
+        this.pokemonType = pokemonType;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s", this.pokemonName, this.pokemonType);
+    }
+}
