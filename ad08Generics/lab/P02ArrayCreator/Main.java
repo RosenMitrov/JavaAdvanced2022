02bf468df806450f463099c422674d2c83931543
RosenMitrov <rdatabase.m@gmail.com>
2022-10-18 10:21:28 +0300

    Added ArrayCreator Main

---
+++ ad08Generics/lab/P02ArrayCreator/Main.java
@@ -0,0 +1,10 @@
+package JavaAdvanced.ad08Generics.lab.P02ArrayCreator;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Integer[] integers = ArrayCreator.create(3, 69);
+
+
+    }
+}
