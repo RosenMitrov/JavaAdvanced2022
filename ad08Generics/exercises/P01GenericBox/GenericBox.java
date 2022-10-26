42e5ff969a28f7f1aed0a34680622a8db4781717
RosenMitrov <rdatabase.m@gmail.com>
2022-10-26 10:56:11 +0300

    Added Generic Box

---
+++ ad08Generics/exercises/P01GenericBox/GenericBox.java
@@ -0,0 +1,26 @@
+package JavaAdvanced.ad08Generics.exercises.P01GenericBox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericBox<T> {
+    private List<T> list;
+
+    public GenericBox() {
+        this.list = new ArrayList<>();
+    }
+
+    public void add(T value) {
+        this.list.add(value);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (T value : this.list) {
+
+            sb.append(String.format("%s: %s%n", value.getClass().getName(), value));
+        }
+        return sb.toString();
+    }
+}
