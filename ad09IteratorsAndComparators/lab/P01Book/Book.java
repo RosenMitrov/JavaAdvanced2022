85ff661b17e143c8484d8474d99ce48aeff8fa6f
RosenMitrov <rdatabase.m@gmail.com>
2023-01-01 20:16:00 +0200

    Iterators and Comparators

---
+++ ad09IteratorsAndComparators/lab/P01Book/Book.java
@@ -0,0 +1,41 @@
+package JavaAdvanced.ad09IteratorsAndComparators.lab.P01Book;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Book {
+    private String title;
+    private int year;
+    private List<String> authors;
+
+    public Book(String title, int year, String... authors) {
+        this.setTitle(title);
+        this.setYear(year);
+        this.setAuthors(authors);
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setAuthors(String... authors) {
+        this.authors = Arrays.asList(authors);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+}
