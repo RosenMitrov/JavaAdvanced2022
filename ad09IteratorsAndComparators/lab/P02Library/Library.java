55c24ff944e904b1af4972362d7cc5d0d27d5830
RosenMitrov <rdatabase.m@gmail.com>
2023-01-02 19:47:55 +0200

    Added Library Library

---
+++ ad09IteratorsAndComparators/lab/P02Library/Library.java
@@ -0,0 +1,32 @@
+package JavaAdvanced.ad09IteratorsAndComparators.lab.P02Library;
+
+import java.util.Iterator;
+
+public class Library<Book> implements Iterable<Book> {
+
+    private Book[] books;
+
+    public Library(Book... books) {
+        this.books = books;
+    }
+
+    @Override
+    public Iterator<Book> iterator() {
+        return new LibraryIterator();
+    }
+
+
+    private final class LibraryIterator implements Iterator<Book> {
+        private int index = 0;
+
+        @Override
+        public boolean hasNext() {
+            return index < books.length;
+        }
+
+        @Override
+        public Book next() {
+            return books[index++];
+        }
+    }
+}
