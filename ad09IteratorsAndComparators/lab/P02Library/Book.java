8005eb36a9d4284287dc76e96d31ac460be0372a
RosenMitrov <rdatabase.m@gmail.com>
2023-01-02 19:47:38 +0200

    Added Library book

---
+++ ad09IteratorsAndComparators/lab/P02Library/Book.java
@@ -0,0 +1,27 @@
+package JavaAdvanced.ad09IteratorsAndComparators.lab.P02Library;
+
+import java.util.List;
+
+public class Book {
+    private String title;
+    private int year;
+    private List<String> authors;
+
+    public Book(String title, int year, String... authors) {
+        this.title = title;
+        this.year = year;
+        this.authors = List.of(authors);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+}
