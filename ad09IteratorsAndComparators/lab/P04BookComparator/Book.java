84e1a71b24661ca96891e40fd070962f8575bc00
RosenMitrov <rdatabase.m@gmail.com>
2023-01-04 18:37:03 +0200

    added

---
+++ ad09IteratorsAndComparators/lab/P04BookComparator/Book.java
@@ -0,0 +1,45 @@
+package JavaAdvanced.ad09IteratorsAndComparators.lab.P04BookComparator;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Book {
+    private String title;
+    private int year;
+    private List<String> authors;
+
+    public Book(String title, int year, String... authors) {
+        this.setTitle(title);
+        this.setYear(year);
+        this.setAuthors(authors);
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setAuthors(String... authors) {
+        this.authors = Arrays.asList(authors);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s -> %d", this.title, this.year);
+    }
+}
