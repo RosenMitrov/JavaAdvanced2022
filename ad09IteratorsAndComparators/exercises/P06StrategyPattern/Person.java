df79e146ae731d92a8316fda51dbe996f3921128
RosenMitrov <rdatabase.m@gmail.com>
2023-01-10 18:38:37 +0200

    Added Comparator by age

---
+++ ad09IteratorsAndComparators/exercises/P06StrategyPattern/Person.java
@@ -0,0 +1,24 @@
+package JavaAdvanced.ad09IteratorsAndComparators.exercises.P06StrategyPattern;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %d", this.name, this.age);
+    }
+}
